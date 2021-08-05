a92e44aba9d0559b9eb2cb4cba8caa26d2313d83
zhangce <z874010534@vip.qq.com>
2021-08-05 14:47:45 +0800

    vue3 suspense

---
+++ vue/vue3/14.Suspense/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <Suspense>
+      <template #default>
+        <async-show></async-show>
+      </template>
+      <template #fallback>
+        <h1>loading.....</h1>
+      </template>
+    </Suspense>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent,onErrorCaptured,reactive,ref,toRefs,watch} from 'vue'
+import AsyncShow from './AsyncShow.vue'
+export default defineComponent({
+  components:{
+    'async-show':AsyncShow
+  },
+  setup() {
+    onErrorCaptured((e:any) => {
+      console.log(e)
+    })
+  },
+})
+</script>
---
+++ vue/vue3/14.Suspense/AsyncShow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+     {{result}}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  async setup() {
+    return new Promise((resolve) => {
+      // 发送请求
+      setTimeout(() => {
+        return resolve({
+          result: 42
+        })
+      }, 3000);
+    })
+  },
+})
+</script>
