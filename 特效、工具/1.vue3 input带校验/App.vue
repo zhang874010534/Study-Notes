6b73b94d88d27d1010682081a3bbc9c1b68fdc93
zhangce <z874010534@vip.qq.com>
2021-08-09 12:03:34 +0800

    vue3+TS input组件 带验证

---
+++ 特效、工具/1.vue3 input带校验/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+    <validate-input :rules="emailRules" v-model="params.email" placeholder="请输入邮箱地址"></validate-input>
+    {{params.email}}
+</template>
+
+<script lang="ts">
+import { defineComponent,reactive} from 'vue'
+import ValidateInput,{RulesProp} from './ValidateInput.vue'
+export default defineComponent({
+  components:{
+    'validate-input':ValidateInput
+  },
+  setup() {
+    const emailRules: RulesProp = [
+      { type:'required', message:'电子邮箱不能为空'},
+      { type:'email', message:'邮箱错误'}
+    ]
+    const params = reactive({
+      email: ''
+    })
+
+    return {
+      emailRules,
+      params
+    }
+  },
+})
+</script>
---
+++ 特效、工具/1.vue3 input带校验/ValidateInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <input 
+    type="text" 
+    @blur="validateInput"
+    :value="inputRef.val"
+    @input="updateValue"
+    v-bind="$attrs"
+  >
+  <div v-if="inputRef.error">{{inputRef.message}}</div>
+</template>
+<script lang="ts">
+import { defineComponent,PropType, reactive } from 'vue'
+interface RuleProp {
+  type: 'required' | 'email';
+  message: string
+}
+export type RulesProp = RuleProp[];
+const emailReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+export default defineComponent({
+  props:{
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
+  },
+  setup(props,context) { 
+    const inputRef = reactive({
+      val: props.modelValue ||'',
+      error: false, 
+      message: ''
+    })
+    const updateValue = (e:any) => {
+      const targetValue = (e.target as HTMLInputElement).value
+      inputRef.val = targetValue
+      context.emit('update:modelValue',targetValue)
+    }
+    const validateInput = () => {
+      if(props.rules){
+        const allPass = props.rules.every((ruleItem) => {
+          let passed = true
+          inputRef.message = ruleItem.message
+          switch (ruleItem.type) {
+            case 'required':
+              passed = inputRef.val.trim() !== ''
+              break;
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break;
+            default: 
+              break;
+          }
+          return passed
+        })
+        inputRef.error = !allPass
+      }
+    }
+    return {
+      inputRef,
+      validateInput,
+      updateValue,
+    }
+  },
+})
+</script>
