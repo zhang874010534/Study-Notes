17dd536c2ed4cecff3c3ad1483758247dcf120d3
zhang <38835461+zhang874010534@users.noreply.github.com>
2021-10-04 12:26:05 +0800

    vue.config.js resolve

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/backstage/' : '/backstage/',
   productionSourceMap: false,
