cc64678db9cae046aa966d9200605b165b227746
zhangce <z874010534@vip.qq.com>
2023-08-20 19:16:48 +0800

    feat: three联合材质 SceneUtils类不在THREE上了需要单独引入

---
+++ WebGL/three/案例/材质/7.联合材质.vue
@@ -0,0 +1,58 @@
+<script setup>
+import * as THREE from 'three';
+import * as SceneUtils from 'three/examples/jsm/utils/SceneUtils'
+// 创建一个场景
+const scene = new THREE.Scene()
+
+// 创建一个相机 视点
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+// 设置相机的位置
+camera.position.set(0, 0, 20)
+
+// 创建一个渲染器
+const renderer = new THREE.WebGLRenderer()
+// 设置渲染器尺寸
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+// 添加一个立方体
+// 定义了一个立方体的对象
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+
+// 创建材质
+const Lambert = new THREE.MeshLambertMaterial({
+  color: 0xff0000
+})
+const basic = new THREE.MeshBasicMaterial({
+  wireframe: true
+})
+const cubeMaterial = new THREE.MeshDepthMaterial()
+
+const cube = new SceneUtils.createMultiMaterialObject(cubeGeometry,[Lambert, basic])
+// console.log(SceneUtils)
+// 添加到场景里
+scene.add(cube)
+
+// 添加灯光
+const spotLight = new THREE.SpotLight(0xffffff)
+spotLight.position.set(-10, 10, 10)
+scene.add(spotLight)
+
+const animation = () => {
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
+  // 渲染
+  renderer.render(scene, camera)
+
+  requestAnimationFrame(animation)
+}
+animation()
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
