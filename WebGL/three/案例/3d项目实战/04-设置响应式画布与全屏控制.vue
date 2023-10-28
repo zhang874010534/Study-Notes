76fc404f95033ca34bd49daa54e15229a86e81f5
zhangce <z874010534@vip.qq.com>
2023-10-28 14:45:58 +0800

    feat: three 设置响应式画布与全屏控制

---
+++ WebGL/three/案例/3d项目实战/04-设置响应式画布与全屏控制.vue
@@ -0,0 +1,76 @@
+<template>
+  <div @click="clickButton" style="position: fixed;left: 0;top: 0;z-index: 2000;background: #fff">全屏</div>
+</template>
+<script setup>
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// 创建一个场景
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const parentMaterial = new THREE.MeshBasicMaterial({color: 0xff0000})
+
+let parentCube = new THREE.Mesh(geometry, parentMaterial)
+parentCube.position.set(-3, 0 ,0)
+// parentCube.scale.set(2, 2, 2)
+
+const cube = new THREE.Mesh(geometry, material)
+cube.position.set(3, 0, 0)
+cube.scale.set(2, 2, 2)
+cube.rotation.x = Math.PI / 4
+
+parentCube.add(cube)
+scene.add(parentCube)
+
+camera.position.z = 5
+camera.position.y = 2
+camera.position.x = 2
+camera.lookAt(0, 0, 0)
+
+// 添加世界坐标辅助器
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+// 添加轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 设置带阻尼的惯性
+controls.enableDamping = true
+// 设置阻尼系数
+controls.dampingFactor = 0.05
+scene.add(controls)
+
+function animate () {
+  controls.update()
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
+}
+animate()
+
+window.addEventListener('resize', () => {
+  // 重置渲染器宽高比
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // 重置相机宽高比
+  camera.aspect = window.innerWidth / window.innerHeight
+
+  // 更新相机投影矩阵
+  camera.updateProjectionMatrix()
+})
+
+const clickButton = () => {
+  renderer.domElement.requestFullscreen()
+}
+</script>
+<style scoped>
+</style>
