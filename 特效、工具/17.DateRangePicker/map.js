63c5db26e6e7401d6b29fc18d82b23ab4af76c50
zhangce <z874010534@vip.qq.com>
2023-11-05 18:55:47 +0800

    feat: 时间选择日、月、季、任意

---
+++ 特效、工具/17.DateRangePicker/map.js
@@ -0,0 +1,17 @@
+export const rangePickerOptions = [{
+  value: 1,
+  label: '按月',
+  type: 'month'
+},{
+  value: 2,
+  label: '按季',
+  type: 'quarter'
+},{
+  value: 3,
+  label: '按年',
+  type: 'year'
+},{
+  value: 4,
+  label: '自定义时间',
+  type: 'custom'
+}]
