1698442c77276792d37ea524b32b8fb90f414ba3
zhangce <38835461+zhang874010534@users.noreply.github.com>
2024-05-19 20:05:22 +0800

    feat: three动画与相机、骨骼动画骨架创建

---
+++ 绘制图形和动画canvas webgl/WebGL/three/案例/three动画与相机/three骨骼动画/three骨骼动画.vue
@@ -0,0 +1,73 @@
+<script setup>
+import * as THREE from 'three'
+// 轨道控件
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+const clock = new THREE.Clock()
+// 创建一个场景
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('#FFFFFF')
+
+// 创建一个相机 视点
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+// 设置相机的位置
+camera.position.set(100, 100, 0)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+
+// 创建一个渲染器
+const renderer = new THREE.WebGLRenderer()
+
+// 设置渲染器尺寸
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+// 轨道
+// const controls = new OrbitControls(camera, renderer.domElement)
+
+// 环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 3) // 创建环境光
+scene.add(ambientLight) // 将环境光添加到场景
+
+const spotLight = new THREE.SpotLight(0xffffff, 5) // 创建聚光灯
+spotLight.position.set(200, 200, 200)
+spotLight.castShadow = true
+scene.add(spotLight)
+
+// 圆柱体
+const geometry = new THREE.CylinderGeometry(2, 2, 40, 8, 12)
+//
+const material = new THREE.MeshPhongMaterial()
+// 创建骨骼
+const bone = new THREE.Bone()
+bone.position.set(0, 20, 0)
+const bone2 = new THREE.Bone()
+bone2.position.set(0, 10, 0)
+bone.add(bone2)
+const bone3 = new THREE.Bone()
+bone3.position.set(0, 10, 0)
+bone2.add(bone3)
+const bone4 = new THREE.Bone()
+bone4.position.set(0, 10, 0)
+bone3.add(bone4)
+const bone5 = new THREE.Bone()
+bone5.position.set(0, 10, 0)
+bone4.add(bone5)
+// 创建 SkinnedMesh
+const mesh = new THREE.SkinnedMesh(geometry, material)
+// 创建骨架
+const skeleton = new THREE.Skeleton([bone, bone2, bone3, bone4, bone5])
+mesh.add(bone)
+// 绑定骨架
+mesh.bind(skeleton)
+scene.add(mesh)
+const animation = () => {
+  // 渲染
+  renderer.render(scene, camera)
+
+  requestAnimationFrame(animation)
+}
+animation()
+</script>
+<template></template>
