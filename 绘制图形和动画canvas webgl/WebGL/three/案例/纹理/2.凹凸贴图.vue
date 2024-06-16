e7d6ebc742d489ee0c3fc32cbff082bf2168c128
zhangce <38835461+zhang874010534@users.noreply.github.com>
2024-06-16 12:23:22 +0800

    feat: three凹凸贴图

---
+++ 绘制图形和动画canvas webgl/WebGL/three/案例/纹理/2.凹凸贴图.vue
@@ -0,0 +1,85 @@
+<script setup>
+import * as THREE from 'three';
+// 创建一个场景
+const scene = new THREE.Scene()
+
+// 创建一个相机 视点
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+// 设置相机的位置
+camera.position.set(0, 0, 20)
+
+// 创建一个渲染器
+const renderer = new THREE.WebGLRenderer()
+// 设置渲染器尺寸
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+// 添加一个立方体
+// 定义了一个立方体的对象
+const geometry = new THREE.BoxGeometry(6, 6, 3)
+
+// 纹理加载器
+const textureLoader = new THREE.TextureLoader()
+textureLoader.load('src/assets/wall.jpg', (texture) => {
+  // mesh.material.map = texture
+  // mesh.material.needsUpdate = true
+  // 创建材质
+  const material  = new THREE.MeshPhongMaterial({
+    // wireframe: true,
+    map: texture
+  })
+
+  const mesh = new THREE.Mesh(geometry, material )
+  mesh.rotation.y = -0.1
+  mesh.rotation.x = -4
+
+  // 添加到场景里
+  scene.add(mesh)
+})
+renderer.setClearColor(0xffffff)
+textureLoader.load('src/assets/wall.jpg', (texture) => {
+  textureLoader.load('src/assets/wall-bump.jpg', (bump) => {
+    // 创建材质
+    const material = new THREE.MeshPhongMaterial({
+      map: texture,
+      bumpMap: bump,
+      bumpScale: 0.2, // 高度
+    })
+
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.rotation.y = -0.5
+    mesh.rotation.x = 4
+    mesh.position.x = 5
+    // 添加到场景里
+    scene.add(mesh)
+  })
+})
+
+
+
+// 添加灯光
+const pointLight = new THREE.PointLight(0xffffff, 1, 100); // 第三个参数是光源的最大距离
+pointLight.position.set(0, 5, 10); // 设置光源位置
+scene.add(pointLight);
+
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // 环境光
+// scene.add(ambientLight);
+// 创建一个方向光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // 第二个参数是光的强度，默认1
+directionalLight.position.set(1, 1, 1); // 设置光源的方向，相对场景原点，默认为(0, 0, 0)
+scene.add(directionalLight); // 将光源添加到场景中
+
+const animation = () => {
+  // 渲染
+  renderer.render(scene, camera)
+  requestAnimationFrame(animation)
+}
+animation()
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
