b2a398e10040c07936a334d3e52b1f267d68160a
zhangce <38835461+zhang874010534@users.noreply.github.com>
2024-07-16 23:10:48 +0800

    feat: 产物源码分析-sourcemap

---
+++ 项目构建/webpack/README.md
@@ -0,0 +1,18 @@
+## 产物源码分析 - sourcemap
+
+```js
+{  
+ "version": 3, // 文件版本
+ "file": "app.js", // 对应文件的名称
+ "mappings": ";;AAAA",
+ "sources": [ // 源文件列表
+ "webpack://webpack-project/./src/test.js"  
+ ],  
+ "names": [], // 源文件变量名和属性名
+ "sourceRoot": "" // 源文件根目录
+}
+```
+
+
+
+
--- 项目构建/webpack/webpack.dev.js
+++ 项目构建/webpack/webpack.dev.js
@@ -25,10 +25,10 @@ const config = {
 
   mode: 'development',
   // mode: 'production',
-  // inline内敛 hidden生成不引入 eval以eval形式生成 nosources不生成源码 cheap省略一些详细信息比如列映射 module生成module信息
+  // inline内敛 hidden生成不引入 eval以eval形式生成 nosources不生成源码(生成sourcemap但是在控制台点报错信息无法看到源码) cheap省略一些详细信息比如列映射 module生成module信息
   // production模式只支持 source-map hidden-source-map nosources-source-map 以及 hidden-nosources-source-map
   // devtool: [inline-|hidden-|eval-][nosources-][cheap-[module-]]source-map
-  // devtool: 'source-map',
+  devtool: 'nosources-source-map',
   module: {
     rules: [
       // {
