1d519b88bc9a699e288e26a393a3cc39b02049c0
zhangce <38835461+zhang874010534@users.noreply.github.com>
2024-07-28 16:47:59 +0800

    feat: 环境贴图立方体相机WebGLCubeRenderTarget

--- 绘制图形和动画canvas webgl/WebGL/three/案例/纹理/4.环境贴图.vue
+++ 绘制图形和动画canvas webgl/WebGL/three/案例/纹理/4.环境贴图.vue
@@ -20,6 +20,7 @@ camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // 创建一个渲染器
 const renderer = new THREE.WebGLRenderer()
+renderer.shadowMap.enabled = true  // 启用阴影映射
 
 // 设置渲染器尺寸
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -70,29 +71,48 @@ scene.add(skyBox);
 const sphereGeometry = new THREE.SphereGeometry(4, 15, 15)
 const cubeBoxGeometry = new THREE.BoxGeometry(5, 5, 5)
 
-// 立方体贴图是和环境一致, 球体是跟随当前环境
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  envMap: new THREE.TextureLoader().load('src/assets/sky/right.jpg'),
-  side: THREE.DoubleSide,
-})
-
 // 通过立方体相机来实现
-const cubeCamera = new THREE.CubeCamera(0.1, 2000, 256)
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
+  format: THREE.RGBFormat,
+  generateMipmaps: true,
+  minFilter: THREE.LinearMipmapLinearFilter,
+})
+const cubeCamera = new THREE.CubeCamera(0.1, 2000, cubeRenderTarget)
 scene.add(cubeCamera)
 
 const sphereMaterial = new THREE.MeshBasicMaterial({
   envMap: cubeCamera.renderTarget.texture,
 })
 
+// 立方体贴图是和环境一致, 球体是跟随当前环境
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  // envMap: new THREE.TextureLoader().load('src/assets/sky/right.jpg'),
+  // side: THREE.DoubleSide,
+  envMap: cubeCamera.renderTarget.texture,
+})
+
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 const cube = new THREE.Mesh(cubeBoxGeometry, cubeMaterial)
+sphere.position.x = 5
+cube.position.x = -5
+sphere.castShadow = true // 球体投射阴影
+sphere.receiveShadow = true // 球体接收阴影
+cube.castShadow = true // 立方体投射阴影
+cube.receiveShadow = true // 立方体接收阴影
 scene.add(sphere)
 scene.add(cube)
 
 const animation = () => {
-  // trackBall.update()
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
   controls.update(clock.getDelta())
-  // 渲染
+  // 更新立方体相机并渲染到其目标纹理
+  sphere.visible = false // 隐藏球体以避免自身渲染到环境贴图中
+  cube.visible = false // 隐藏立方体以避免自身渲染到环境贴图中
+  cubeCamera.update(renderer, scene)
+  sphere.visible = true // 恢复球体的可见性
+  cube.visible = true // 恢复立方体的可见性
+
   renderer.render(scene, camera)
   requestAnimationFrame(animation)
 }
